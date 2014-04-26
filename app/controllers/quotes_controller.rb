6ff56839cb58cdf6671c3026e5d2614a5bdd2372
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-27 00:41:40 +0200

    Before adding the socket layer to noodles.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,26 @@
 require 'pry'
+
 class QuotesController < Noodles::Controller
   def a_quote
     @name = "Something pretty."
     @user_agent = request.user_agent
-    render 'a_quote'
+    render html: 'a_quote'
+  end
+
+  def erb
+    @name = "Something pretty."
+    @user_agent = request.user_agent
+    render erb: 'a_quote'
+  end
+
+  def html
+    @name = "Something pretty."
+    @user_agent = request.user_agent
+    render html: 'a_quote'
+  end
+
+  def text
+    '<h1>No Quote</h1>'
   end
 
   def exception
