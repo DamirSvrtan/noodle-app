06ed75c9aaf45e342f1c2b01536d79c52ab8ae10
Damir Svrtan <damir.svrtan@gmail.com>
2014-04-27 16:30:31 +0200

    Mini Chatter example working.

--- app/handlers/chat_handler.rb
+++ app/handlers/chat_handler.rb
@@ -1,14 +1,18 @@
+require 'json'
 class ChatHandler < Noodles::Websocket::Handler
   def on_open env
     add_connection self
-    broadcast "hello"
+    msg = { username: params(env)['username'], message: 'connected' }
+    broadcast msg.to_json
   end
 
   def on_message env, msg
-
+    broadcast msg
   end
 
   def on_close env
     remove_connection self
+    msg = { username: params(env)['username'], message: 'disconnected' }
+    broadcast msg.to_json
   end
 end
