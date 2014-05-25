69b1c4a99b880ed2f06375153aa512510f223c78
Damir Svrtan <damir.svrtan@gmail.com>
2014-05-25 02:27:22 +0200

    Using jquery, showing all users.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class ChatController < BaseController
   def index
+    return redirect_to "/" unless signed_in?
     slim :index
   end
 end
--- app/views/base_view.rb
+++ app/views/base_view.rb
@@ -1,3 +1,6 @@
 class BaseView
   include AuthHelper
+  def users
+    User.all - [current_user]
+  end
 end
